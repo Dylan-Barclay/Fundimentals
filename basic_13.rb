bd24dccc0f79d8c1845e2bce6d37696ac7023e46
Dylan-Barclay <dr.barclay99@gmail.com>
2021-08-03 19:27:12 -0500

    completing basic 13

--- basic_13.rb
+++ basic_13.rb
@@ -1,21 +1,14 @@
-# # 1
-# for i in 1..225 
-#     puts "#{i}" 
-# end
+# 1
+(1..255).each { |i| puts i }
 
-# # 2
-# for i in 0..225 
-#     puts "#{i}" if i % 2 == 1
-# end
+# 2
+(1..255).each { |i| puts i if i.odd? }
 
-#3
-# x = 0
-# for i in 0..225 
-#     x += i
-#     puts "New number:#{i} Sum:#{x}" 
-# end
+# 3
+sum = 0
+(0..255).each { |i| puts "New Number: #{i} Sum: #{sum += i}"}
 
-#4
+# 4
 array =[1,3,5,7,9]
 array.each{ |x| puts x}
 
@@ -23,5 +16,27 @@ array.each{ |x| puts x}
 puts array.max
 
 #6
-array.each{ |x| puts x}
-y += x
+puts array.reduce(:+) / array.length.to_f
+
+#7
+y = []
+(1..255).each { |i| y << i if i.odd? }
+
+#8
+x = 3
+puts array.count { |elem| elem > x }
+
+#9
+puts array.map! { |elem| elem * elem }
+
+#10
+puts array.each_index { |index| array[index] = 0 if array[index] < 0 }
+
+#11
+{ max: array.max, min: array.min, average: array.reduce(:+) / array.length.to_f }
+
+#12
+array.rotate!(1)[array.length-1] = 0
+
+#13
+puts array.each_index { |index| array[index] = "Dojo" if array[index] < 0 }

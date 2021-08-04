f123c7827b256c0be56fdd899183ee4129c5358d
Dylan-Barclay <dr.barclay99@gmail.com>
2021-08-03 19:04:42 -0500

    adding arrays and basic 13

---
+++ basic_13.rb
@@ -0,0 +1,27 @@
+# # 1
+# for i in 1..225 
+#     puts "#{i}" 
+# end
+
+# # 2
+# for i in 0..225 
+#     puts "#{i}" if i % 2 == 1
+# end
+
+#3
+# x = 0
+# for i in 0..225 
+#     x += i
+#     puts "New number:#{i} Sum:#{x}" 
+# end
+
+#4
+array =[1,3,5,7,9]
+array.each{ |x| puts x}
+
+#5
+puts array.max
+
+#6
+array.each{ |x| puts x}
+y += x
